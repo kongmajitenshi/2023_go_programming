9dbbb33534f4c8faef9a4374ddedc5d1a0503765
eogml <eogml0624@gmail.com>
2023-09-25 15:12:34 +0900

    230925_10

--- week05/main.go
+++ week05/main.go
@@ -10,9 +10,14 @@ import (
 func main() {
 	fmt.Print("Input Score : ")
 	reader := bufio.NewReader(os.Stdin)
-	inputScore, err := reader.ReadString('\n') // option2
+	inputScore, err := reader.ReadString('\n')
 	if err != nil {
 		log.Fatal(err)
+	}
+	if inputScore >= 90 { // mismatched types string and untyped int
+		grade := "A grade!"
+	} else {
+		grade := "BCDE grade~"
 	}
 	fmt.Println(inputScore)
 }
