f750cc5da5cceed1a0396ba8e6579296276fea6a
eogml <eogml0624@gmail.com>
2023-09-25 15:46:12 +0900

    230925_14

--- week05/main.go
+++ week05/main.go
@@ -1,11 +1,36 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
+	"log"
 	"math/rand"
+	"os"
+	"strconv"
+	"strings"
+	"time"
 )
 
 func main() {
-	dice := rand.Intn(6) + 1
-	fmt.Println(dice)
+	rand.Seed(time.Now().Unix()) // get the current date and time as an integer
+	answer := rand.Intn(100) + 1 // randon integer number between 1 and 100
+	fmt.Println("Guess Number Game~")
+	fmt.Println(answer)
+
+	reader := bufio.NewReader(os.Stdin)
+	fmt.Print("Input guess number : ")
+	inputNumberString, err := reader.ReadString('\n')
+	if err != nil {
+		log.Fatal(err)
+	}
+	inputNumberString = strings.TrimSpace(inputNumberString)
+	inputNumber, err := strconv.Atoi(inputNumberString)
+	if err != nil {
+		log.Fatal(err)
+	}
+	if inputNumber < answer {
+		fmt.Println("up") // Answer is higher
+	} else if inputNumber > answer {
+		fmt.Println("down") // Answer is lower
+	}
 }
