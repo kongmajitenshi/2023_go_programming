73dbeb2494330c1da031e153dd30b747fccce86f
kongmajitenshi <dksk3504@gmail.com>
2023-10-23 14:55:16 +0900

    231023_4

--- week09/main.go
+++ week09/main.go
@@ -3,16 +3,40 @@ package main
 import (
 	"fmt"
 )
+func swap(n1 int, n2 int) {
+	temp := n1
+	n1 := n2
+	n2 := temp
+}
 
-// 포인터
-func double(n *int) {
-	*n = *n * 2
-}
 func main() {
-	var a int = 6
-	double(&a)
-	fmt.Println(a)
+	a := 10
+	b := 20
+
+	c := &a // var c *int = &a
+	fmt.Printf("%T\n", c)
+
+	fmt.Println(a, b)
+	swap(a,b)
+	fmt.Println(a,b)
 }
+
+
+// package main
+
+// import (
+// 	"fmt"
+// )
+
+// // 포인터
+// func double(n *int) {
+// 	*n = *n * 2
+// }
+// func main() {
+// 	var a int = 6
+// 	double(&a)
+// 	fmt.Println(a)
+// }
 
 
 // package main
