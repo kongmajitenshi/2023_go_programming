facad1142c080e88c55d88f53c59097e7d12bd5b
kongmajitenshi <dksk3504@gmail.com>
2023-10-23 14:49:34 +0900

    231023_3

--- week09/main.go
+++ week09/main.go
@@ -5,11 +5,28 @@ import (
 )
 
 // 포인터
-func double(n int) {
-	n = n * 2
+func double(n *int) {
+	*n = *n * 2
 }
 func main() {
 	var a int = 6
-	double(a)
+	double(&a)
 	fmt.Println(a)
 }
+
+
+// package main
+
+// import (
+// 	"fmt"
+// )
+
+// // 포인터
+// func double(n int) {
+// 	n = n * 2
+// }
+// func main() {
+// 	var a int = 6
+// 	double(a)
+// 	fmt.Println(a)
+// }
