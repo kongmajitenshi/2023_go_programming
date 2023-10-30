bc3642ca39fc7ac67b2bc700995f77f1946654ed
kongmajitenshi <dksk3504@gmail.com>
2023-10-30 14:53:04 +0900

    231030_3

--- week10/src/mymath/mymath.go
+++ week10/src/mymath/mymath.go
@@ -1,5 +1,6 @@
 package mymath
 
+// func myAbs(number int) int // 외부 파일에 함수를 공개하기 위해선 함수의 첫글자를 대문자로 해준다.
 func MyAbs(number int) int {
 	if number < 0 {
 		return number * -1
@@ -7,4 +8,12 @@ func MyAbs(number int) int {
 		return number
 	}
 
+}
+
+func MyPower(num1 int, num2 int) int {
+	result := 1
+	for i := 1; i <= num2; i++ {
+		result = result * num1
+	}
+	return result
 }
