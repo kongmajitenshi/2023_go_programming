160283037866febed320b9a11cf9a56f32184f11
kongmajitenshi <dksk3504@gmail.com>
2023-11-13 14:23:16 +0900

    231113_3

--- week12/main.go
+++ week12/main.go
@@ -9,6 +9,9 @@ func main() {
 	// s := make([]int, 5) // 단축 연산자. make함수를 이용해 슬라이스 생성, 메모리 할당, zero value값 적용
 
 	s := []int{0, 0, 0, 0, 0} // 슬라이스 리터럴을 이용해 슬라이스 생성, 메모리 할당, 초기화 진행
+
+	s[4] = 99
+	s[2] = 91
 	for _, value := range s {
 		fmt.Println(value)
 	}
