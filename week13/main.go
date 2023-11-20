b5fa7f794a624bbd9a95f9569723b2aa0fca9a87
kongmajitenshi <dksk3504@gmail.com>
2023-11-20 14:09:28 +0900

    231120-2

--- week13/main.go
+++ week13/main.go
@@ -4,6 +4,10 @@ import "fmt"
 
 func main() {
 	var a []string
-	fmt.Printf("%#v\n", a)
+	var b []bool
+	a = make([]string, 4, 5)
+
+	fmt.Printf("%#v %#v\n", a, b)
 	fmt.Println(a, len(a), cap(a))
+	fmt.Println(b, len(b), cap(b))
 }
