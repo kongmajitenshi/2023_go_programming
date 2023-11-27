6931a1d22c882e3c62d3afc6f189841daa42f092
kongmajitenshi <dksk3504@gmail.com>
2023-11-27 14:43:51 +0900

    231127-8

--- week14/main.go
+++ week14/main.go
@@ -2,13 +2,22 @@ package main
 
 import "fmt"
 
+func status(name string) {
+	balls := map[string]int{"성기훈": 20, "오일남": 0}
+	ball, exists := balls[name]
+	// fmt.Println(ball, exists)
+	if !exists {
+		fmt.Println(name, "님은 참여자가 아닙니다.")
+	} else if ball < 1 {
+		fmt.Println(name, "님은", balls[name], "개로 탈락입니다.")
+	} else {
+		fmt.Println(name, "님은", balls[name], "개로 통과입니다.")
+	}
+}
+
 func main() {
-	// balls := make(map[string]int)
-	// fmt.Printf("%#v\n", balls)
-	// var balls map[string]int
-	balls := make(map[string]int)
-	fmt.Printf("%#v\n", balls)
-	balls["성기훈"] = 20
-	fmt.Println(balls["성기훈"])
-	fmt.Println(balls["오일남"])
+	status("오일남")
+	status("강철")
+	status("성기훈")
+
 }
