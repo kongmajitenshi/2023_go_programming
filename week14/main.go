f107db64cdd48c8b2ad5b036fd04b569ebafb0ec
kongmajitenshi <dksk3504@gmail.com>
2023-11-27 14:19:40 +0900

    231127-2

--- week14/main.go
+++ week14/main.go
@@ -5,7 +5,7 @@ import "fmt"
 func main() {
 	var games map[int]string
 	games = make(map[int]string)
-
+	// 추가(append / map에는 append메소드 없음)
 	games[456] = "성기훈"
 	games[218] = "박해수"
 	games[067] = "강새벽"
@@ -15,5 +15,11 @@ func main() {
 
 	for _, v := range games {
 		fmt.Println(v)
+	}
+
+	games[101] = "장덕수" // update
+	delete(games, 199) // delete
+	for k, v := range games {
+		fmt.Println(k, v)
 	}
 }
