2fff17b4daab33ef4f86b184e767a87427983e6e
kongmajitenshi <dksk3504@gmail.com>
2023-11-27 14:37:54 +0900

    231127-7

--- week14/main.go
+++ week14/main.go
@@ -3,36 +3,12 @@ package main
 import "fmt"
 
 func main() {
-	// var games map[int]string
-	// games = make(map[int]string)
-	// games := make(map[int]string)
-	games := map[int]string{
-		456: "성기훈",
-		218: "박해수",
-		067: "강새벽",
-		001: "오일남",
-		199: "알리",
-		101: "아이오아이",
-	}
-	// 추가(append / map에는 append메소드 없음)
-	// games[456] = "성기훈"
-	// games[218] = "박해수"
-	// games[067] = "강새벽"
-	// games[001] = "오일남"
-	// games[199] = "알리"
-	// games[101] = "아이오아이"
-
-	// fmt.Println(games[100])
-	// name, ok := games[100]
-	name, ok := games[101]
-	fmt.Println(name, ok)
-	for _, v := range games {
-		fmt.Println(v)
-	}
-
-	games[101] = "장덕수" // update
-	delete(games, 199) // delete
-	for k, v := range games {
-		fmt.Println(k, v)
-	}
+	// balls := make(map[string]int)
+	// fmt.Printf("%#v\n", balls)
+	// var balls map[string]int
+	balls := make(map[string]int)
+	fmt.Printf("%#v\n", balls)
+	balls["성기훈"] = 20
+	fmt.Println(balls["성기훈"])
+	fmt.Println(balls["오일남"])
 }
